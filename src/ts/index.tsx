13f5faf9105f44a85d35d6dffbf5962fa8287caf
martinli <youjenli@gmail.com>
2019-03-27 23:11:31 +0800

    整合 prismjs 套件

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -4,6 +4,18 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { HelloWorld } from './component/helloworld'; 
 import * as Countable from 'countable';
+/*
+雖然 Prism 透過全域變數提供模組，照以下 typescript 官方文件的說法，應該使用 declare 關鍵字搭配 namespace 定義較為洽當，
+但是 @types/prismjs 卻是用許多 export 輸出 Prism 模組對外曝露的接口，因此這裡要用下面這種語法才能順利載入 Prism 到 TypeScript。
+ */
+import * as Prism from 'prismjs';
+/*
+Prism 的語言模組之擴充方式是到目前所在的領域去找 Prism 變數並為其加掛功能，也就是它一樣要透過全域變數完成。
+因此載入的做法是直接執行模組檔案，不需要再取為任何變數或名稱。
+*/
+import '../../node_modules/prismjs/components/prism-java';
+
+import '../../node_modules/prismjs/plugins/line-numbers/prism-line-numbers';
 
 ReactDOM.render(<HelloWorld />, 
     document.getElementById("react-root"));
@@ -12,3 +24,5 @@ Countable.count(document.getElementById('headline'), (counter) => {
     document.getElementById('word-count')
         .innerHTML =`標題共有: ${counter.characters} 字，${counter.characters} 字元`;
 });
+
+Prism.highlightElement(document.getElementById('prismjs-demo'));
