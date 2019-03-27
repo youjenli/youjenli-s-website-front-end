40f56d51cf4c1ac8a649e00e5c91647c95df6818
martinli <youjenli@gmail.com>
2019-03-27 13:56:59 +0800

    整合countable函式庫以計算字數

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,7 +1,14 @@
+/// <reference path="../../node_modules/countable/index.d.ts" />
 import * as React from 'react';
 //使用模板之前一定要載入 react 套件，否則 tsc 會報錯。
 import * as ReactDOM from 'react-dom';
 import { HelloWorld } from './component/helloworld'; 
+import * as Countable from 'countable';
 
 ReactDOM.render(<HelloWorld />, 
     document.getElementById("react-root"));
+
+Countable.count(document.getElementById('headline'), (counter) => {
+    document.getElementById('word-count')
+        .innerHTML =`標題共有: ${counter.characters} 字，${counter.characters} 字元`;
+});
