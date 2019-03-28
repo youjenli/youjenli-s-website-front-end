caeca80f3dae27bebfd7944b7676c8b489b31103
martinli <youjenli@gmail.com>
2019-03-28 21:23:37 +0800

    整合進 navigo 套件

--- src/ts/component/helloworld.tsx
+++ src/ts/component/helloworld.tsx
@@ -2,11 +2,7 @@
 import * as React from 'react';
 
 export class HelloWorld extends React.Component {
-    render() {
-        return (
-            <div>
-            歡迎來到祐任的個人網站！
-            </div>            
-        )
-    }
+    render () {
+        return (<div>歡迎來到祐任的個人網站！</div>);            
+    };
 }
--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,11 +1,8 @@
 /// <reference path="../../node_modules/countable/index.d.ts" />
-import * as React from 'react';
-//使用模板之前一定要載入 react 套件，否則 tsc 會報錯。
-import * as ReactDOM from 'react-dom';
-import { HelloWorld } from './component/helloworld'; 
 import * as Countable from 'countable';
 import { AxiosResponse } from 'axios';
 import fetchResource from './axios-example';
+import './router';
 /*
 雖然 Prism 透過全域變數提供模組，照以下 typescript 官方文件的說法，應該使用 declare 關鍵字搭配 namespace 定義較為洽當，
 但是 @types/prismjs 卻是用許多 export 輸出 Prism 模組對外曝露的接口，因此這裡要用下面這種語法才能順利載入 Prism 到 TypeScript。
@@ -19,12 +16,11 @@ import '../../node_modules/prismjs/components/prism-java';
 
 import '../../node_modules/prismjs/plugins/line-numbers/prism-line-numbers';
 
-ReactDOM.render(<HelloWorld />, 
-    document.getElementById("react-root"));
-
-Countable.count(document.getElementById('headline'), (counter) => {
-    document.getElementById('word-count')
-        .innerHTML =`標題共有: ${counter.characters} 字，${counter.characters} 字元`;
+window.addEventListener('DOMContentLoaded', () => {
+    Countable.count(document.getElementById('headline'), (counter) => {
+        document.getElementById('word-count')
+            .innerHTML =`標題共有: ${counter.characters} 字，${counter.characters} 字元`;
+    })
 });
 
 fetchResource()
