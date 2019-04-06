dcd12438e5bfce3fd96019a718356ce736f25a94
martinli <youjenli@gmail.com>
2019-04-06 17:29:49 +0800

    使用prefix來使標題列能夠在更多瀏覽器上固定顯示在畫面上方

--- src/ts/service/featureDetection.ts
+++ src/ts/service/featureDetection.ts
@@ -16,13 +16,29 @@ export function isPlaceHolderOfInputSupported():boolean {
 export function isStickyPositionSupported():boolean {
     if (stickyPositionSupported === null) {
         if (isCssSupportFunctionAvailable){
-            stickyPositionSupported = CSS.supports('position', 'sticky');
+            stickyPositionSupported = 
+                CSS.supports('position', 'sticky') ||
+                CSS.supports('position', '-webkit-sticky') ||
+                CSS.supports('position', '-moz-sticky') ||
+                CSS.supports('position', '-ms-sticky') ||
+                CSS.supports('position', '-o-sticky');
         } else {
             const test = document.createElement('div');
             const mStyle = test.style;
-            mStyle.cssText = "position:sticky";
-            stickyPositionSupported = mStyle.position.indexOf("sticky") !== -1;
-        }        
+            mStyle.cssText = `
+                position: -webkit-sticky;
+                position: -moz-sticky;
+                position: -ms-sticky;
+                position: -o-sticky;
+                position: sticky;
+                `;
+            stickyPositionSupported = 
+                mStyle.position.indexOf("sticky") !== -1 ||
+                mStyle.position.indexOf("-webkit-sticky") !== -1 ||
+                mStyle.position.indexOf("-moz-sticky") !== -1 ||
+                mStyle.position.indexOf("-ms-sticky") !== -1 ||
+                mStyle.position.indexOf("-o-sticky") !== -1 ;
+        }
     }
     return stickyPositionSupported;
 }
