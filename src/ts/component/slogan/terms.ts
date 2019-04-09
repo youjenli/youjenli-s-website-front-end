e4fc20b3d243b36acdbaace0a4690471a0831a7f
martinli <youjenli@gmail.com>
2019-04-09 21:51:32 +0800

    在slogan簡單自介句中使用全形逗號

--- src/ts/component/slogan/terms.ts
+++ src/ts/component/slogan/terms.ts
@@ -1,4 +1,4 @@
 export let greetingMsg = "您好~";
 export let myName = "我是李祐任";
 export let welcomeMsg = "歡迎來到我的個人網站！";
-export let desc = "我是台灣的資訊工作者, 全端開發者, 喜歡組織ＩＴ解決方案, 對能夠提高工作效率的工具,技術與方法都很感興趣, 喜歡介紹這些東西給朋友和同事";
+export let desc = "我是台灣的資訊工作者，全端開發者，喜歡組織ＩＴ解決方案，對能夠提高工作效率的工，技術與方法都很感興趣，喜歡介紹這些東西給朋友和同事。";
