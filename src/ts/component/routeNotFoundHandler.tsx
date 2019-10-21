3783f2227407d024cd071efa324eb4401c16a860
martinli <youjenli1124@gmail.com>
2019-10-21 14:46:28 +0800

    確認所有頁面跑例外流程時，都有事將送給首頁元件的錯誤訊息編碼。除此之外也說明這樣做的原因。

--- src/ts/component/routeNotFoundHandler.tsx
+++ src/ts/component/routeNotFoundHandler.tsx
@@ -7,5 +7,10 @@ export const eventHandlersOfExceptionFlow = routeEventHandlerOfHome;
 export const routeNotFoundHandler = () => {
     const path = router.lastRouteResolved().url;
     const query = `${queryParametersOfHome.ERROR_MSG}=${encodeURIComponent(terms.pageNotFound + ' ' + terms.systemDoesNotServeContentCorrespondingToProvidedPath(path))}`;
+    /*
+        註：這邊的請求參數一定要編碼。這是因為 renderHomePage 處理函式會解請求參數的編碼。
+        至於 renderHomePage 要解請求參數編碼的原因是──似乎因為瀏覽器的歷史紀錄 API 收到路徑之後會自動編碼的關係，
+        所以實驗發現當使用者令 navigo 導向特定路徑時，處理函式會接到編碼後的請求參數，因此一定要解碼才能使用。
+    */
     renderHomePage(query);
 };
--- src/ts/component/tag/routerHandler.tsx
+++ src/ts/component/tag/routerHandler.tsx
@@ -67,6 +67,11 @@ const prepareToPresentHomePageWithErrorMsg = (done, errorMsg:string) => {
     isProgressAbnormal = true;
     renderHomePageWithErrorMsg = () => {
         const query = `${queryParametersOfHome.ERROR_MSG}=${encodeURIComponent(errorMsg)}`;
+        /*
+            註：這邊的請求參數一定要編碼。這是因為 renderHomePage 處理函式會解請求參數的編碼。
+            至於 renderHomePage 要解請求參數編碼的原因是──似乎因為瀏覽器的歷史紀錄 API 收到路徑之後會自動編碼的關係，
+            所以實驗發現當使用者令 navigo 導向特定路徑時，處理函式會接到編碼後的請求參數，因此一定要解碼才能使用。
+        */
         renderHomePage(query);
     }
     routeEventHandlersOfHome.before(done);
