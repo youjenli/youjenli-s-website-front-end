ef6fec5a3db925a181ac98a8b5a67c6f14dc4357
youjenli <youjenli1124@gmail.com>
2020-01-28 18:38:10 +0800

    移除程式進入點的無用函式 navigateToHomeWithErrorMessage。

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -95,20 +95,3 @@ export function performSearch(keyword:string, page:number) {
     }
     router.navigate(route);
 }
-
-export function navigateToHomeWithErrorMessage(msg:string):void {
-    /*
-       註: 這裡呼叫 navigate 的狀況與 performSearch 相同。
-       雖然 Navigo 的 navigate 函式在拿 route 去更新瀏覽器的歷史狀態之前不會特別編碼連結，
-       但是似乎因為瀏覽器的歷史紀錄 API 接到路徑之後會自動編碼的關係，所以實際運作發現這邊不用手動編碼路徑，
-       否則當路徑有部分中文字時，反而會導致瀏覽器自動編碼後的連結與原本的目標連結不同。
-    */
-    const queryStr = `${queryParametersOfHome.ERROR_MSG}=${msg}`;
-    if (queryStr.length >= 2048) {
-        /*
-            如果錯誤訊息太長，則保留適當的長度並去掉多餘訊息。
-        */
-        queryStr.substring(0, 2048);
-    }
-    router.navigate(`/?${queryStr}`);
-}
