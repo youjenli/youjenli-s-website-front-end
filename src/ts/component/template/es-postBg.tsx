81121ff9773cd9b3fa84a7a81ebe6a9f2e4ae890
youjenli <youjenli1124@gmail.com>
2020-05-02 07:37:39 +0800

    修正外接螢幕版發表物頁面上的目錄無法點擊的問題。

--- src/ts/component/template/es-postBg.tsx
+++ src/ts/component/template/es-postBg.tsx
@@ -67,7 +67,11 @@ export default class PostBackgroundOnExternalScreen extends React.Component<Prop
         if (this.props.toc) {
             const styleOfToc = {
                 width:`${this.props.toc.width}px`,
-                padding:`0 ${this.props.toc.paddingLeftRight}px`
+                padding:`0 ${this.props.toc.paddingLeftRight}px`,
+                /* 外接螢幕版的目錄必須套用高的 zIndex，否則要是它右邊有清單這種不是靜態定位的元素，
+                   那麼使用者將無法點擊與清單同列的目錄項目。
+                */
+                zIndex:this.props.baseZIndex + 100
             }
             const styleOfTocTitle = {
                 fontSize:`${this.props.toc.title.fontSize}px`,
