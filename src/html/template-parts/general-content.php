9ae07c6ef8715a290386b2cae1101796c66f8b4b
youjenli <youjenli1124@gmail.com>
2020-05-02 07:18:01 +0800

    為前端加入 promise 墊片函式庫

--- src/html/template-parts/general-content.php
+++ src/html/template-parts/general-content.php
@@ -5,4 +5,5 @@
 ?>
 <div id="react-root"></div>
 <script type="text/javascript" src="<?php echo get_theme_file_uri( $pathOfMainJsOutputFile ); ?>"></script>
-<?php //https://developer.wordpress.org/reference/functions/path_join/ ?>
+<script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
+<?php // 以上函式庫是用來自動偵測執行環境是否支援 es6 promise 函式庫並視情況產生實作。 ?>
