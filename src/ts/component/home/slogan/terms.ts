f3040db6dc8966b80868932297ae73f5a82837b6
youjenli <youjenli1124@gmail.com>
2020-05-03 22:56:50 +0800

    調整首頁自介短句，使它更符合我的需求。

--- src/ts/component/home/slogan/terms.ts
+++ src/ts/component/home/slogan/terms.ts
@@ -1,4 +1,4 @@
 export let greetingMsg = "您好~";
 export let myName = "我是李祐任！";
 export let welcomeMsg = "歡迎來到我的個人網站！";
-export let desc = "我是台灣的資訊工作者，全端開發者，喜歡組織ＩＴ解決方案，對能夠提高工作效率的工，技術與方法都很感興趣，喜歡介紹這些東西給朋友和同事。";
+export let desc = "我是 Web 全端軟體開發人員，喜歡組織ＩＴ解決方案。我喜歡學習並應用任何能夠提高工作效率的工具、技術與方法，然後再介紹這些東西給朋友和同事。";
